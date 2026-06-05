d0479565e553bd0409321b234982d714087d0bc8
jiwonniddaaa <jiwonniddaaa02@gmail.com>
2026-06-05 14:19:15 +0900

    fix: 노드 불러오게 수정

--- image_modules/image_understanding_node.py
+++ image_modules/image_understanding_node.py
@@ -122,7 +122,7 @@ def image_understanding_node(state: Dict[str, Any]) -> Dict[str, Any]:
     if image_type == "hand_sketch":
         route = "sketch_agent_node"
     elif image_type == "reference_image":
-        route = "reference_agent_node"
+        route = "reference_agent"
     else:
         route = "unsupported_image"
 
